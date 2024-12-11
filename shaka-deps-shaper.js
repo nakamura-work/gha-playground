34e755262f251e47d0f6be1685945a9839453851
t.nakamura <nakamura@webstream.co.jp>
2024-12-11 16:45:38 +0900

    fix: shaka-deps-shaper.js

--- shaka-deps-shaper.js
+++ shaka-deps-shaper.js
@@ -1,52 +1,81 @@
+// @ts-check
+
 const fs = require("fs");
-const readline = require("readline");
 
-const stream = fs.createReadStream("./out.d.ts", "utf8");
+let dts = fs.readFileSync("./out.d.ts", "utf-8");
 
-const reader = readline.createInterface({ input: stream });
 
 // Step1. Except below namespace of "ಠ_ಠ.clutz"
-const lines = [];
-reader.on("line", (data) => {
-    if (/ ಠ_ಠ.clutz /.test(data)) reader.close();
-    lines.push(data);
+dts = removeNamespaceBlock(dts);
+
+// Step2. Except "ಠ_ಠ.clutz."
+dts = dts.replace(/ಠ_ಠ.clutz./g, "");
+
+// Step3. Except extra output by clutz
+dts = dts.replace(/extends GlobalEventTarget extends GlobalEventTarget/g, "extends GlobalEventTarget");
+
+// Step4. Except local path
+dts = dts.replace(/\/\/ Generated from (.*)/g, (m, /** @type {string}*/p) => {
+    const filePath = p.substring(p.indexOf("shaka"));
+    return `// Generated from ${filePath}`;
 });
 
-reader.on("close", () => {
-    let dts = lines.slice(0, -1).join("\n");
+// Step5. Replace clutz specific Object alias
+const map = {
+    "GlobalDate": "Date",
+    "GlobalElement": "Element",
+    "GlobalError": "globalThis.Error",
+    "GlobalEvent": "Event",
+    "GlobalEventTarget": "EventTarget",
+    "GlobalFloat32Array": "Float32Array",
+    "GlobalFloat64Array": "Float64Array",
+    "GlobalObject": "Object",
+    "GlobalStorage": "Storage",
+    "webCrypto.CryptoKey": "CryptoKey",
+};
+Object.entries(map).forEach(([from, to]) => {
+    dts = dts.replace(new RegExp(from, "g"), to);
+});
 
-    // Step2. Except "ಠ_ಠ.clutz."
-    dts = dts.replace(/ಠ_ಠ.clutz./g, "");
-
-    // Step3. Except local path
-    dts = dts.replace(/\/\/ Generated from (.*)/g, (m, /** @type {string}*/p) => {
-        const filePath = p.substring(p.indexOf("shaka"));
-        return `// Generated from ${filePath}`;
-    });
-
-    // Step4. Replace clutz specific Object alias
-    const map = {
-        "GlobalDate": "Date",
-        "GlobalElement": "Element",
-        "GlobalError": "globalThis.Error",
-        "GlobalEvent": "Event",
-        "GlobalEventTarget": "EventTarget",
-        "GlobalFloat32Array": "Float32Array",
-        "GlobalFloat64Array": "Float64Array",
-        "GlobalObject": "Object",
-        "GlobalStorage": "Storage",
-    };
-    Object.entries(map).forEach(([from, to]) => {
-        dts = dts.replace(new RegExp(from, "g"), to);
-    });
-
-    // Step5. Add export
-    dts += `
+// Step5. Add export
+dts += `
 
 // Shaped by WebStream Corp.
 export default shaka;
-export type { shakaExtern };
 `;
 
-    fs.writeFileSync("./shaka/shaka-player.d.ts", dts);
-});
+fs.writeFileSync("./shaka/shaka-player.d.ts", dts);
+
+/**
+ * 
+ * @param {string} input 
+ * @returns 
+ */
+function removeNamespaceBlock(input) {
+    const startPattern = /^declare namespace ಠ_ಠ\.clutz(?:\.u2f)?\s*{/;
+    let output = '';
+    let stack = 0;
+    let inBlock = false;
+    for (let i = 0; i < input.length; i++) {
+        const match = input.slice(i).match(startPattern);
+        if (!inBlock && match) {
+            inBlock = true;
+            stack++;
+            i += match[0].length;
+            continue;
+        }
+        if (inBlock && input.at(i) === '{') {
+            stack++;
+        } else if (inBlock && input.at(i) === '}') {
+            stack--;
+            if (stack === 0) {
+                inBlock = false;
+            }
+        } else {
+            if (!inBlock) {
+                output += input.at(i);
+            }
+        }
+    }
+    return output;
+}
